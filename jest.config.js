1c78d3235544d381c5c9b4b4da06518bd542d258
Minor Gordon <minorg@server.fake>
2024-01-16 17:44:17 -0500

    bootstrap project

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+const esModules = [];
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  transformIgnorePatterns: [`node_modules/(?!${esModules.join("|")})`],
+};
