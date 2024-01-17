afa56d2a72e5045a68eafc55d3dba22e74374075
Minor Gordon <minorg@server.fake>
2024-01-17 15:31:14 -0500

    fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ const jestConfig: JestConfigWithTsJest = {
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
+  testRegex: ".*\\.test\\.ts",
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
